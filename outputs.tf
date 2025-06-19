dd3c83c10e9d57cbc2ac2b9257f11ad078983d0a
Nagaraj <nagarajuande255@gmail.com>
2025-06-19 11:18:26 +0530

    terraform

---
+++ outputs.tf
@@ -0,0 +1,3 @@
+output "azs_info" { # This output will provide the availability zones used in the VPC module
+    value = module.vpc.azs_info # this will refer to aws vpc data source in the vpc module
+}
