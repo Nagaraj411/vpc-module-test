97c6a2f3bc8c530d36046cde5990e1588bd21301
Nagaraj <nagarajuande255@gmail.com>
2025-06-23 19:37:26 +0530

    terraform

--- main.tf
+++ main.tf
@@ -1,11 +1,11 @@
 module "vpc" {
-  source = "../terraform-aws-vpc"
+  #source = "../terraform-aws-vpc"
   # project             = "roboshop"
   # environment         = "dev"
   # public_subnet_cidrs = ["10.0.1.0/24", "10.0.2.0/24"]
   # private_subnet_cidrs = ["10.0.11.0/24", "10.0.12.0/24"]
   # database_subnet_cidrs = ["10.0.21.0/24", "10.0.22.0/24"]
-
+source = "git::https://github.com/Nagaraj411/vpc-module-test.git?ref=main"
   project               = var.project
   environment           = var.environment
   public_subnet_cidrs   = var.public_subnet_cidrs
